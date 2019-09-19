2cf23ffccd0dc9afc869def7e9b1b99581295920
ToothColdWoo <upup1810@naver.com>
2019-09-19 21:06:02 +0900

    Make firt step

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import Customer from "./components/Customer"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const customer = {
+  'name':'홍길동',
+  'birthday':'961003',
+  'gender':'남자',
+  'job':'대학생'
+}
+
+class App extends Component{
+  render(){
+    return(
+      <Customer 
+      name = {customer.name}
+      birthday = {customer.birthday}
+      gender = {customer.gender}
+      job = {customer.job}
+      />
+    )
+  }
 }
 
 export default App;
