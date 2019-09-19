1ab91ff5e4b55050b662213ea3ab9c502f761362
ToothColdWoo <upup1810@naver.com>
2019-09-19 21:29:18 +0900

    Make another Customers.

--- src/App.js
+++ src/App.js
@@ -1,22 +1,35 @@
 import React, {Component} from 'react';
 import Customer from "./components/Customer"
 
-const customer = {
-  'name':'홍길동',
-  'birthday':'961003',
-  'gender':'남자',
-  'job':'대학생'
-}
+const customers = [
+  {
+    'id':1,
+    'name':'홍길동',
+    'birthday':'961003',
+    'gender':'남자',
+    'job':'대학생'
+  },
+  {
+    'id':2,
+    'name':'홍길',
+    'birthday':'961004',
+    'gender':'남자',
+    'job':'대학생'
+  },
+]
+
 
 class App extends Component{
   render(){
     return(
-      <Customer 
-      name = {customer.name}
-      birthday = {customer.birthday}
-      gender = {customer.gender}
-      job = {customer.job}
-      />
+      <div>
+        {customers.map(c=>{
+          return <Customer key={c.id} id ={c.id}
+          name={c.name}
+          birthday={c.birthday}
+          gender = {c.gender} />
+        })}
+      </div>
     )
   }
 }
--- src/components/Customer.js
+++ src/components/Customer.js
@@ -4,13 +4,33 @@ class Customer extends Component{
     render(){
         return (
             <div>
-                <h2>{this.props.name}</h2>
-                <p>{this.props.birthday}</p>
-                <p>{this.props.gender}</p>
-                <p>{this.props.job}</p>
+                <CustomerProfile id = {this.props.id} name = {this.props.name} />
+                <CustomerInfo birthday={this.props.birthday} gender={this.props.gende}r job = {this.props.job} />
             </div>
         )
     }
 }
 
+class CustomerProfile extends Component{
+    render(){
+        return (
+            <div>
+                <h2>{this.props.name}({this.props.id})</h2>
+            </div>
+        )
+    }
+}
+
+class CustomerInfo extends Component{
+    render(){
+        return (
+            <div>
+                <p>{this.props.birthday}</p>
+                <p>{this.props.gender}</p>
+                <p>{this.props.job}</p>
+
+            </div>
+        )
+    }
+}
 export default Customer;
